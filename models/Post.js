ceb62ecc362043f70fa2432ad68dd110787afeee
Craig Leslie <cleslie25@gmail.com>
2022-04-11 23:41:44 -0500

    File structure and models

---
+++ models/Post.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {}
+
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    post_title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    post_content: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    post_author: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    // post_comment: {
+    //     type: DataTypes.TEXT,
+    //     allowNull: true,
+    // },
+    user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'user',
+          key: 'id',
+        },
+      },
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'post',
+  }
+);
+
+module.exports = Post;
